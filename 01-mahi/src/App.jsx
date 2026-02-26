895fa349f3cc5c753e56fc3db7c740129f2124a8
Akash Chauhan <akashchauhan087@gmail.com>
2026-02-26 22:43:20 +0530

    ui created used by props

---
+++ 01-mahi/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './Component/Card'
+import Navbar from './Component/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ 01-mahi/src/Component/Card.jsx
@@ -0,0 +1,14 @@
+
+const user = 'Mahima';
+
+const Card = () => {
+    return (
+     <div className="card">
+            <h1>{user}</h1>
+            <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, accusantium</h2>
+     </div>
+)
+
+}
+
+export default Card
